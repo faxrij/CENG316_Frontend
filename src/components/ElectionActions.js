294ec698ae96d2ef0a03d9ffec6cb3ca67023fe4
Fahri <cfaxri@mail.ru>
2023-05-22 13:10:11 +0300

    added Create Election Page and styling

--- src/components/ElectionActions.js
+++ src/components/ElectionActions.js
@@ -1,22 +1,19 @@
-// ElectionActions.js
-
 export const handleVote = (electionName) => {
-    // Handle the vote button click for a specific election
-    console.log(`Vote button clicked for ${electionName}`);
-  };
-  
-  export const handleCreateElection = () => {
-    // Handle the create election button click
-    console.log('Create Election button clicked');
-  };
-  
-  export const handleEditElection = (electionName) => {
-    // Handle the edit button click for a specific election
-    console.log(`Edit button clicked for ${electionName}`);
-  };
-  
-  export const handleDeleteElection = (electionName) => {
-    // Handle the delete button click for a specific election
-    console.log(`Delete button clicked for ${electionName}`);
-  };
-  
+	// Handle the vote button click for a specific election
+	console.log(`Vote button clicked for ${electionName}`);
+};
+
+export const handleCreateElection = () => {
+	// Handle the create election button click
+	console.log("Create Election button clicked");
+};
+
+export const handleEditElection = (electionName) => {
+	// Handle the edit button click for a specific election
+	console.log(`Edit button clicked for ${electionName}`);
+};
+
+export const handleDeleteElection = (electionName) => {
+	// Handle the delete button click for a specific election
+	console.log(`Delete button clicked for ${electionName}`);
+};
