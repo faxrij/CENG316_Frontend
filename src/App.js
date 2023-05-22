835cac922d597c7de3a14e4ef1c892ccd6f16d5f
zeynepbektas <zeynepbektas>
2023-05-22 14:44:09 +0300

    login page added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
 import Login from './components/pages/Login';
+import Announcement from "./components/Announcement";
+import CreateElectionPage from "./components/CreateElectionPage";
+import Elections from "./components/Elections";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Routes>
-          <Route path='/' exact Component={Login} />
-        </Routes>
-      </Router>
+				<Navbar />
+				<Routes>
+					<Route path="/" exact />
+					<Route path="/announcements" element={<Announcement />} />
+					<Route path="/election" element={<Elections />} />
+					<Route path="/create-election" element={<CreateElectionPage />} />
+				</Routes>
+			</Router>
     </div>
   );
 }
