b642cee4ef76a1d41ff1fa64477237dc96c84d74
zeynepbektas <zeynepbektas>
2023-06-06 23:10:26 +0300

    geliştirme

--- src/components/pages/Elections.js
+++ src/components/pages/Elections.js
@@ -1,35 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import "../Elections.css";
-import { getElectionsData } from "../../mocks/mockElections"; //
+import { getElectionsData } from "../../mocks/mockElections";
 import ConfirmationModal from "../ConfirmationModal";
-import { Link, Route, Routes } from "react-router-dom";
+import { Link, Route, Routes, useNavigate } from "react-router-dom"; // Import useNavigate
 
 import ElectionService from '../ElectionService';
 
 import {
-	handleVote,
 	handleCreateElection,
 	handleEditElection,
 	handleDeleteElection,
 } from "../ElectionActions";
 
-import CreateElectionPage from "./CreateElectionPage"; // Import the CreateElectionPage component
+import CreateElectionPage from "./CreateElectionPage";
 
 const Elections = () => {
   const [electionsData, setElectionsData] = useState([]);
   const electionService = new ElectionService();
 
-	const [showModal, setShowModal] = useState(false);
-	const [selectedElection, setSelectedElection] = useState(null);
-	const [showCreateElection, setShowCreateElection] = useState(false); // State for showing CreateElectionPage
+  const [showModal, setShowModal] = useState(false);
+  const [selectedElection, setSelectedElection] = useState(null);
+  const [showCreateElection, setShowCreateElection] = useState(false);
 
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
+  const navigate = useNavigate(); // Add useNavigate hook
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const data = await electionService.getElections();
+        const data = await getElectionsData();
         setElectionsData(data);
         setIsLoading(false);
       } catch (error) {
@@ -49,85 +49,90 @@ const Elections = () => {
     return <div className="error-message">Error: {error.message}</div>;
   }
   
-  // Check the user role here (replace "student" with the actual role property)
   const userRole = localStorage.getItem('userRole');
+  
 
-	const handleConfirmDelete = () => {
-		console.log(`Deleting election: ${selectedElection}`);
-		setShowModal(false);
-	};
+  const handleVote = (electionName) => {
+    console.log(`Vote button clicked for ${electionName}`);
+    navigate('/vote'); // Navigate to "/vote" route
+  };
 
-	const handleCancelDelete = () => {
-		setShowModal(false);
-	};
+  const handleConfirmDelete = () => {
+    console.log(`Deleting election: ${selectedElection}`);
+    setShowModal(false);
+  };
 
-	const handleDeleteElection = (electionName) => {
-		setShowModal(true);
-		setSelectedElection(electionName);
-	};
+  const handleCancelDelete = () => {
+    setShowModal(false);
+  };
 
-	const handleCreateElection = () => {
-		setShowCreateElection(true);
-	};
+  const handleDeleteElection = (electionName) => {
+    setShowModal(true);
+    setSelectedElection(electionName);
+  };
 
-	return (
-		<div className="elections-container">
-			<h1 className="elections-heading">Elections</h1>
-			{userRole === "Admin" && ( // Only render the Create Election button for non-student roles
-				<div className="elections-actions">
-					<Link to="/create-election" className="create-election-button">
-						Create Election
-					</Link>
-				</div>
-			)}
-			<div className="elections-list">
-				{electionsData.map((election, index) => (
-					<div key={index} className="election-card">
-						<h2 className="election-title">{election.name}</h2>
-						<div className="election-details">
-							<p className="election-date">Start Date: {election.startDate}</p>
-							<p className="election-date">End Date: {election.endDate}</p>
-							<p className="election-location">
-								Department: {election.departmentName}
-							</p>
-						</div>
-						<button
-							className="vote-button"
-							onClick={() => handleVote(election.name)}
-						>
-							Vote
-						</button>
-						{userRole === "Admin" && ( // Only render the Edit and Delete buttons for Admin roles
-							<>
-								<button
-									className="edit-button"
-									onClick={() => handleEditElection(election.name)}
-								>
-									Edit
-								</button>
-								<button
-									className="delete-button"
-									onClick={() => handleDeleteElection(election.name)}
-								>
-									Delete
-								</button>
-							</>
-						)}
-					</div>
-				))}
-			</div>
-			{showModal && (
-				<ConfirmationModal
-					message={`Are you sure you want to delete the election: ${selectedElection}?`}
-					onConfirm={handleConfirmDelete}
-					onCancel={handleCancelDelete}
-				/>
-			)}
-			<Routes>
-				<Route path="/create-election" component={CreateElectionPage} />
-			</Routes>
-		</div>
-	);
+  const handleCreateElection = () => {
+    setShowCreateElection(true);
+  };
+
+  return (
+    <div className="elections-container">
+      <h1 className="elections-heading">Elections</h1>
+      {userRole === "Admin" && (
+        <div className="elections-actions">
+          <Link to="/create-election" className="create-election-button">
+            Create Election
+          </Link>
+        </div>
+      )}
+      <div className="elections-list">
+        {electionsData.map((election, index) => (
+          <div key={index} className="election-card">
+            <h2 className="election-title">{election.name}</h2>
+            <div className="election-details">
+              <p className="election-date">Start Date: {election.startDate}</p>
+              <p className="election-date">End Date: {election.endDate}</p>
+              <p className="election-location">
+                Department: {election.departmentName}
+              </p>
+            </div>
+            <button
+              className="vote-button"
+              onClick={() => handleVote(election.name)}
+            >
+              Vote
+            </button>
+            {userRole === "Admin" && (
+              <>
+                <button
+                  className="edit-button"
+                  onClick={() => handleEditElection(election.name)}
+                >
+                  Edit
+                </button>
+                <button
+                  className="delete-button"
+                  onClick={() => handleDeleteElection(election.name)}
+                >
+                  Delete
+                </button>
+              </>
+            )}
+          </div>
+        ))}
+      </div>
+      {showModal && (
+        <ConfirmationModal
+          message={`Are you sure you want to delete the election: ${selectedElection}?`}
+          onConfirm={handleConfirmDelete}
+          onCancel={handleCancelDelete}
+        />
+      )}
+      <Routes>
+        <Route path="/create-election" component={CreateElectionPage} />
+      </Routes>
+    </div>
+  );
 };
 
 export default Elections;
