6d2f54d6fe1e53328662060b2d8028a487fe412d
Fahri <cfaxri@mail.ru>
2023-06-15 23:43:26 +0300

    announcement func

--- src/components/pages/Announcement.js
+++ src/components/pages/Announcement.js
@@ -7,6 +7,8 @@ const Announcement = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
   const [showCreatePopup, setShowCreatePopup] = useState(false);
+  const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
+  const [selectedAnnouncementId, setSelectedAnnouncementId] = useState(null);
 
   const userRole = localStorage.getItem("userRole");
 
@@ -29,6 +31,36 @@ const Announcement = () => {
 
     fetchAnnouncements();
   }, []);
+
+  const handleDeleteAnnouncement = (announcementId) => {
+    // Show the delete confirmation dialog
+    setSelectedAnnouncementId(announcementId);
+    setShowDeleteConfirmation(true);
+  };
+
+  const confirmDeleteAnnouncement = async () => {
+    try {
+      const response = await fetch(`http://164.90.217.39:5000/api/announcement/${selectedAnnouncementId}`, {
+        method: 'DELETE',
+      });
+      if (response.ok) {
+        // Perform any additional UI updates or actions after successful deletion
+        const updatedAnnouncements = announcements.filter(
+          (announcement) => announcement.id !== selectedAnnouncementId
+        );
+        setAnnouncements(updatedAnnouncements);
+      } else {
+        throw new Error('Failed to delete announcement');
+      }
+    } catch (error) {
+      // Handle the error
+      console.error(error);
+    } finally {
+      // Close the confirmation dialog
+      setShowDeleteConfirmation(false);
+    }
+  };
+  
 
   if (isLoading) {
     return <div className="loading-message">Loading...</div>;
@@ -54,15 +86,37 @@ const Announcement = () => {
           </span>
         )}
       </h1>
-      {announcements.map((announcement, index) => (
-        <div key={index} className="announcement-card">
+      {announcements.map((announcement) => (
+        <div key={announcement.id} className="announcement-card">
           <h2 className="announcement-title">{announcement.name}</h2>
           <p className="announcement-description">{announcement.description}</p>
           <div className="announcement-details">
             <p className="announcement-creator">Created by: {announcement.createdBy}</p>
+            {userRole === 'Admin' && (
+              <button
+                className="delete-announcement-button"
+                onClick={() => handleDeleteAnnouncement(announcement.id)}
+              >
+                Delete
+              </button>
+            )}
           </div>
         </div>
       ))}
+
+      {/* Modal Confirmation Dialog */}
+      {showDeleteConfirmation && (
+        <div className="modal">
+          <div className="modal-content">
+            <h3>Confirmation</h3>
+            <p>Are you sure you want to delete this announcement?</p>
+            <div className="modal-actions">
+              <button onClick={confirmDeleteAnnouncement}>Yes</button>
+              <button onClick={() => setShowDeleteConfirmation(false)}>No</button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
